2bb76985a393b8eafd59e47fdd4071d79c7d3031
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-07-29 10:02:33 -0700

    Add IO routines for reading/writing lists of words.

---
+++ include/io/io.hpp
@@ -0,0 +1,35 @@
+#ifndef IO_HPP_
+#define IO_HPP_
+
+#include <string>
+#include <vector>
+
+namespace morse {
+namespace io {
+
+/** @brief Read a list of words from STDIN. */
+std::vector<std::string> ReadWords();
+
+/**
+ * @brief Read a list of words from the file at \p filepath
+ *
+ * @throws std::runtime_error When \p filepath is inaccessible.
+ *
+ * @returns A vector of words found in the parameter file in the order in which
+ *          they appear.
+ */
+std::vector<std::string> ReadWords(const std::string& filepath);
+
+/**
+ * @brief Write \p data to the file at \p filepath
+ *
+ * @details If \p filepath exists, its contents will be overwritten.
+ *
+ * @throws std::runtime_error When \p filepath is inaccessible.
+ */
+void WriteToFile(const std::string& data, const std::string& filepath);
+
+}  // namespace io
+}  // namespace morse
+
+#endif
---
+++ src/io/io.cc
@@ -0,0 +1,58 @@
+#include "io/io.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace morse {
+namespace io {
+
+static std::vector<std::string> ParseWords(const std::string& data) {
+    std::istringstream iss(data);
+    std::vector<std::string> words;
+    std::copy(std::istream_iterator<std::string>(iss),
+              std::istream_iterator<std::string>(), std::back_inserter(words));
+    return words;
+}
+
+std::vector<std::string> ReadWords() {
+    std::string data;
+    std::string line;
+    while (std::cin >> line) {
+        data += line;
+    }
+    return ParseWords(data);
+}
+
+std::vector<std::string> ReadWords(const std::string& filepath) {
+    if (!std::filesystem::exists(filepath)) {
+        std::runtime_error("error '" + filepath + "' does not exist");
+    }
+
+    std::ifstream input_file(filepath);
+    if (!input_file) {
+        throw std::runtime_error("error '" + filepath + "' cannot be opened");
+    }
+
+    std::stringstream buffer;
+    buffer << input_file.rdbuf();
+
+    return ParseWords(buffer.str());
+}
+
+void WriteToFile(const std::string& data, const std::string& filepath) {
+    std::ofstream output_file(filepath);
+    if (!output_file) {
+        throw std::runtime_error("error '" + filepath + "' cannot be opened");
+    }
+
+    output_file << data;
+}
+
+}  // namespace io
+}  // namespace morse
